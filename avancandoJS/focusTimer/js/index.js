7448b7be8ddedb98f847fe92c75dd89c1a28d2a3
Lucas Muniz <lucasrmuniz@outlook.com.br>
2023-04-01 17:09:43 -0300

    upload focus timer 2.0

--- avancandoJS/focusTimer/js/index.js
+++ avancandoJS/focusTimer/js/index.js
@@ -2,17 +2,15 @@
 import Constrols from "./controls.js"
 import Timer from "./timer.js"
 import Sound from "./sounds.js"
+import Events from "./events.js"
 import {
   buttonPlay,
   buttonPause,
   buttonStop,
   buttonSet,
-  buttonSoundOn,
-  buttonSoundOff,
   minutesDisplay,
   secondsDisplay,
 } from "./elements.js"
-
 
 const controls = Constrols({
   buttonPlay,
@@ -21,52 +19,12 @@ const controls = Constrols({
   buttonStop,
 })
 
-const sound = Sound()
-
 const timer = Timer({
   minutesDisplay,
   secondsDisplay,
   resetControls: controls.reset,
 })
 
-buttonPlay.addEventListener('click', function() {
-  controls.play()
-  timer.countDown()
-  sound.pressButton()
-})
+const sound = Sound()
 
-buttonPause.addEventListener('click', function() {
-  controls.pause()
-  timer.hold()
-  sound.pressButton()
-
-})
-
-buttonStop.addEventListener('click', function() {
-  controls.reset()
-  timer.reset()
-  sound.pressButton()
-
-})
-
-buttonSoundOn.addEventListener('click', function() {
-  buttonSoundOn.classList.add('hide')
-  buttonSoundOff.classList.remove('hide')
-  sound.bgAudio.play()
-})
-
-buttonSoundOff.addEventListener('click', function() {
-  buttonSoundOn.classList.remove('hide')
-  buttonSoundOff.classList.add('hide')
-  sound.bgAudio.pause()
-})
-
-buttonSet.addEventListener('click', function(){
-  let newMinutes = controls.getMinutes()
-  if (!newMinutes) {
-    reset()
-    return
-  }
-  timer.updateDisplay(newMinutes, 0)
-  timer.updateMinutes(newMinutes)
-})
+Events({controls, timer, sound})
