62ca0eac76727e9f6af56fd7a8908dfd581262e1
LucasRaphaelMuniz <lucasrmuniz@outlook.com.br>
2023-05-09 21:36:17 -0300

    updated class

--- nodejs/api/src/server.js
+++ nodejs/api/src/server.js
@@ -1,13 +1,16 @@
 require("express-async-errors");
 
+const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-app.use(express.json())
+migrationsRun();
 
+app.use(express.json())
 app.use(routes);
+
 
 app.use((error, req, res, next) => {
   if(error instanceof AppError) {
@@ -24,6 +27,7 @@ app.use((error, req, res, next) => {
     message: "Internal sever error"
   })
 });
+
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
