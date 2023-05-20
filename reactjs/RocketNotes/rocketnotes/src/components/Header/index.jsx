d8f86bd58764cec0a2fb902b5b973757898852dc
LucasRaphaelMuniz <lucasrmuniz@outlook.com.br>
2023-05-20 10:44:52 -0300

    update ract

---
+++ reactjs/RocketNotes/rocketnotes/src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Profile, Logout } from "./styles";
+
+export function Header(){
+    return(
+        <Container>
+            <Profile>
+                <img 
+                    src="https://github.com/LucasRaphaelMuniz.png" 
+                    alt="Foto do usuário"                
+                />
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Lucas Muniz</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
+        </Container>
+    )
+}
